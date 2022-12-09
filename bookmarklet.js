27a858b163287b755d1baf6d9b7546f48d8452c1
projec-devil <115358339+projec-devil@users.noreply.github.com>
2022-12-09 10:57:20 -0500

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,1 @@
-javascript:(function () {var a = document.createElement('script');a.src = 'https://projec-devil.github.io/script.js';document.body.appendChild(a);}())
+javascript:(function () {var a = document.createElement('script');a.src = 'https://raw.githubusercontent.com/projec-devil/projec-devil.github.io/main/script.js';document.body.appendChild(a);}())
